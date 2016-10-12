ee627657caf667af048cd910c27a3ebd04a03347
Anton <toxa.simply@gmail.com>
2016-10-12 17:54:30 +0300

    New version

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ module.exports = zipFile => {
         resultJsonFile = './data.json';
 
   let firstline = '',
+      lineNr = 0,
       lastLineNr = 0;
 
   var LineByLineReader = require('line-by-line');
+  var Converter = require("csvtojson").Converter;
+  let JSONStream = require('JSONStream');
+  var csvConverter = new Converter({
+    workerNum:4,
+    constructResult: false,
+    delimiter: '|',
+    toArrayString: true,
+    trim: true,
+    ignoreEmpty: true
+  });
 
   new Promise((resolve, reject) => {
 
@@ -31,139 +42,77 @@ module.exports = zipFile => {
         files = files.map(file => `./unzipped/${file}`);
 
         concat(files, csvFile, () => {
-          let file = fs.readFileSync(csvFile, {encoding: 'utf-8'});
+          let readStream = fs.createReadStream(csvFile);
 
-          firstline = file.split('\n')[0];
+          csvConverter.on("record_parsed",function(resultRow,rawRow,rowIndex){
+            lastLineNr = rowIndex;
+          });
 
-          // Determining last line
-          file.split('\n').forEach((i, indx) => lastLineNr = indx + 1);
+          let writeStream = fs.createWriteStream(resultJsonFile);
 
-          resolve();
+          readStream.pipe(csvConverter).pipe(writeStream).on('close', () => resolve());
         });
       });
     });
   }).then(() => {
 
-    let lineNr = 0,
-        headers   = [],
-        /**
-         * The function that transforms data from .csv to .json
-         * @return {data.json} Creates data.json file
-         */
-        transformFunc = () => {
-          new Promise(res => {
-            fs.appendFile(resultJsonFile, '[', err => err && console.log(err));
+      let writeStream = fs.createWriteStream('RESULT.json');
 
-            csv
-              .fromString(firstline, {delimiter: '|'})
-              .on('data', data => headers = data);
+      var getStream = function () {
+        var jsonData = 'data.json',
+            stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
+            parser = JSONStream.parse('*');
+            return stream.pipe(parser);
+      };
+      let obj = {};
 
-            let lr = new LineByLineReader(csvFile);
+      getStream()
+        .pipe(es.mapSync(data => {
 
-            lr.on('line', function (line) {
-            	// pause emitting of lines...
-            	lr.pause();
+          lineNr++;
 
-              lineNr++;
+          obj = {
+            "name": data.name,
+            "phone": data.phone,
+            "person": {
+              "firstName": data.first_name,
+              "lastName": data.last_name
+            },
+            "amount": parseInt(data.amount),
+            "date": data.date.split('/').reverse().join('-'),
+            "costCenterNum": data.cc
+          };
 
-              firstline.includes(line) && lastLineNr--;
-
-              if (lineNr > 1 && !firstline.includes(line)) {
-                let obj = {};
-
-                csv
-                  .fromString(line, {delimiter: '|', headers: headers})
-                  .on('error', err => console.log(err))
-                  .on('data', data => {
-                    obj = {
-                      "name": `${data.first_name} ${data.last_name}`,
-                      "phone": data.phone,
-                      "person": {
-                        "firstName": data.first_name,
-                        "lastName": data.last_name
-                      },
-                      "amount": parseInt(data.amount),
-                      "date": data.date.split('/').reverse().join('-'),
-                      "costCenterNum": data.cc
-                    };
-
-                    if (lineNr === lastLineNr + 2) {
-                      fs.appendFile(resultJsonFile, `${JSON.stringify(obj)}`, err => err && console.log(err));
-                    } else {
-                      fs.appendFile(resultJsonFile, `${JSON.stringify(obj)}, `, err => err && console.log(err));
-                    }
-                });
-              }
-
-              lr.resume();
-            });
-
-            lr.on('end', function () {
-              res();
-              // console.log(lastLineNr);
-            });
+          if (lineNr - 1 === lastLineNr) {
+            return `${JSON.stringify(obj)}]`;
+          } else if (lineNr === 1) {
+            return `[${JSON.stringify(obj)}, `;
+          } else {
+            return `${JSON.stringify(obj)}, `;
+          }
+        }))
+        .on('end', () => {
+          console.log('end');
+          console.log(lineNr);
+          console.log(lastLineNr);
+        })
+        .pipe(writeStream);
 
 
-            // let stream = fs.createReadStream(csvFile)
-            //   .pipe(es.split())
-            //   .pipe(es.mapSync(line => {
-            //       stream.pause();
-            //
-            //       lineNr++;
-            //
-            //       if (lineNr > 1 && !firstline.includes(line)) {
-            //         csv
-            //           .fromString(line, {delimiter: '|', headers: headers})
-            //           .on('error', err => console.log(err))
-            //           .on('data', data => {
-            //             let obj = {
-            //               "name": `${data.first_name} ${data.last_name}`,
-            //               "phone": data.phone,
-            //               "person": {
-            //                 "firstName": data.first_name,
-            //                 "lastName": data.last_name
-            //               },
-            //               "amount": parseInt(data.amount),
-            //               "date": data.date.split('/').reverse().join('-'),
-            //               "costCenterNum": data.cc
-            //             };
-            //
-            //           fs.appendFile(resultJsonFile, `${JSON.stringify(obj)}, `);
-            //           });
-            //       }
-            //
-            //       stream.resume();
-            //     })
-            //     .on('error', err => console.log('EventStream-ERROR', err))
-            //     .on('end', () => {
-            //       fs.appendFile(resultJsonFile, ']');
-            //       console.log(lastLineNr);
-            //
-            //       // fs.unlink(csvFile, () => console.log('Done!'));
-            //     }));
-          }).then(() => {
-            setTimeout(() => {fs.appendFile(resultJsonFile, ']', err => err && console.log(err))}, 2000);
-            // fs.appendFile(resultJsonFile, ']', err => err && console.log(err));
-          });
-        };
-
-
-    // fs.unlink('unziped', err => console.log(err)); //Removing folder
-
-    /**
-     * Remove old data.json file if it exists or create the new one
-     */
-    fs.stat(resultJsonFile, (err, stats) => {
-      if (err) {
-
-        if (err.code === 'ENOENT' ) {
-          return transformFunc();
-        }
-
-        return console.error(err);
-      }
-
-      fs.unlink(resultJsonFile, () => transformFunc());
-    });
+    // /**
+    //  * Remove old data.json file if it exists or create the new one
+    //  */
+    // fs.stat(resultJsonFile, (err, stats) => {
+    //   if (err) {
+    //
+    //     if (err.code === 'ENOENT') {
+    //       return transformFunc();
+    //     }
+    //
+    //     return console.error(err);
+    //   }
+    //
+    //   fs.unlink(resultJsonFile, () => transformFunc());
+    // });
   }).catch(err => console.log(err));
 };
